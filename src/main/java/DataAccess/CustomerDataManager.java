7c41c801159d2a527b204a9a252005608dd36ff7
Luke <Luke@DESKTOP-GF82PT6>
2020-05-10 19:44:20 -0500

    Customer Data Manager

---
+++ src/main/java/DataAccess/CustomerDataManager.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataAccess;
+
+import OrderSystem.Customer;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Luke
+ */
+public class CustomerDataManager {
+    private ArrayList<Customer> dataStore;
+    
+    public CustomerDataManager() {
+        this.dataStore = new ArrayList<Customer>();
+    }
+    public ArrayList<Customer> getAll() {
+        return dataStore;
+    }
+    public void create(Customer customerToCreate) {
+        this.dataStore.add(customerToCreate);
+    }
+}
