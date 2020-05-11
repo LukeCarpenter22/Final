2a667c97e8a5ce392caa80c5c13de2e8120cef5d
Luke <Luke@DESKTOP-GF82PT6>
2020-05-10 19:45:21 -0500

    Product Data Manager

---
+++ src/main/java/DataAccess/ProductDataManager.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataAccess;
+
+import OrderSystem.Product;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Luke
+ */
+public class ProductDataManager {
+    private ArrayList<Product> dataStore;
+    
+    public ProductDataManager() {
+        this.dataStore = new ArrayList<Product>();
+    }
+    public ArrayList<Product> getAll() {
+        return dataStore;
+    }
+    public void create(Product productToCreate) {
+        this.dataStore.add(productToCreate);
+    }
+}
